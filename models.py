d0fd3ca28a08193f3357d7ff093c6baf057ec923
Fabio Landin <flandin1990@gmail.com>
2025-07-30 23:16:59 -0300

    Criando primeira tabelas inicias e banco no sqlite

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
-
+from sqlalchemy_utils.types import ChoiceType
 
 #cria conexao com banco de dados
 db = create_engine("sqlite:///banco.db")
@@ -29,5 +29,48 @@ class Usuario(Base):
         self.admin = admin
 
 #Produto
+class Produto(Base):
+    __tablename__ = "produtos"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    descricao = Column("descricao", String)
+    preco = Column("preco", Float)
+
+    def __init__(self, nome, descricao, preco):
+        self.nome = nome
+        self.descricao = descricao
+        self.preco = preco
+
 #Pedido
+class Pedido(Base):
+    __tablename__ = "pedidos"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", String)
+    usuario = Column("usuario", ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
+
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.status = status
+        self.preco = preco
+
 #Itens Pedido
+class ItemPedido(Base):
+    __tablename__ = "itens_pedido"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+    produto = Column("produto", ForeignKey("produtos.id"))
+
+    def __init__(self, quantidade, preco_unitario, pedido, produto):
+        self.quantidade = quantidade
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
+        self.produto = produto
+
+
+#Cria efetivamente o banco de dados
