ef7627dc870d24ba0d39d77ea1d3146f03573885
Fabio Landin <flandin1990@gmail.com>
2025-08-04 21:41:48 -0300

    Rota para cancelar pedidos, e ajuste para somente admins criarem contas

--- auth_routes.py
+++ auth_routes.py
@@ -35,16 +35,17 @@ async def autenticar():
     return {"mensagem": "Você acessou a rota de autenticação"}
 
 @auth_router.post("/criar_conta")
-async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
-    """ Rota para criar uma nova conta de usuário. """
-    usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
-    if usuario:
-        return HTTPException(status_code=400, detail="Usuário já existe com este email")
-    else:
-        senha_criptografada = bcryp_context.hash(usuario_schema.senha)
-        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
-        session.add(novo_usuario)
-        session.commit()
+async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
+    # Só admin pode criar usuário
+    if not usuario.admin:
+        raise HTTPException(status_code=401, detail="Você não tem autorização para criar usuários")
+    usuario_existente = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
+    if usuario_existente:
+        raise HTTPException(status_code=400, detail="Usuário já existe com este email")
+    senha_criptografada = bcryp_context.hash(usuario_schema.senha)
+    novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
+    session.add(novo_usuario)
+    session.commit()
     return {"mensagem": f"Conta criada com sucesso {usuario_schema.email}"}
 
 
--- order_routes.py
+++ order_routes.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from dependencies import pegar_sessao
+from dependencies import pegar_sessao, verificar_token
 from schemas import PedidoSchema
-from models import Pedido
+from models import Pedido, Usuario
 
-order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
+order_router = APIRouter(prefix="/pedidos", tags=["pedidos"], dependencies=[Depends(verificar_token)])
 
 @order_router.get("/")
 async def pedidos():
@@ -18,3 +18,15 @@ async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(p
     session.add(novo_pedido)
     session.commit()
     return {"mensagem": f"Pedido criado com sucesso para o usuário ID: {novo_pedido.id}"}
+
+@order_router.post("/pedido/cancelar/{id_pedido}")
+async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
+    """ Rota para cancelamento de pedidos."""
+    pedido = session.query(Pedido).filter(Pedido.id == id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado")
+    if not usuario.admin or usuario.id != pedido.usuario:
+        raise HTTPException(status_code=401, detail="Voce não tem autorização para cancelar este pedido")
+    pedido.status = "CANCELADO"
+    session.commit()
+    return {"mensagem": f"Pedido ID: {id_pedido} cancelado com sucesso", "pedido": pedido}
