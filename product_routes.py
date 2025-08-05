7d16f8f7f02ddb8101711504f893154f53363baf
Fabio Landin <flandin1990@gmail.com>
2025-08-04 22:00:20 -0300

    somente admins podem criar produtos

--- product_routes.py
+++ product_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
-from models import Produto
-from dependencies import pegar_sessao
+from models import Produto, Usuario
+from dependencies import pegar_sessao, verificar_token
 from schemas import ProdutoSchema
 from sqlalchemy.orm import Session
 
@@ -15,8 +15,11 @@ async def produtos():
     return {"mensagem": "Você acessou a rota de produtos"}
 
 @product_router.post("/criar_produto")
-async def criar_produto(produto_schema: ProdutoSchema, session: Session = Depends(pegar_sessao)):
+async def criar_produto(produto_schema: ProdutoSchema, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
     """ Rota para criação de produtos. """
+    # Só admin pode criar produtos
+    if not usuario.admin:
+        raise HTTPException(status_code=401, detail="Você não tem autorização para criar produtos")
     novo_produto = Produto(produto_schema.nome, produto_schema.descricao, produto_schema.preco)
     session.add(novo_produto)
     session.commit()
